c710a83df78838f9e2c095e6048eb0c0871266e5
Michael J. Spencer <bigcheesegs@gmail.com>
2011-08-15 03:13:38 -0400

    Add support for version 121.

--- l2crypt.cpp
+++ l2crypt.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
 #include <array>
+#include <cstdint>
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <sstream>
 
 #ifdef WIN32
 # include <io.h>
@@ -39,16 +41,41 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  std::array<char, 28> target_ver =
+  std::array<char, 22> target_ver =
   { 0x4C, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x65, 0x00,
     0x61, 0x00, 0x67, 0x00, 0x65, 0x00, 0x32, 0x00,
-    0x56, 0x00, 0x65, 0x00, 0x72, 0x00, 0x31, 0x00,
-    0x31, 0x00, 0x31, 0x00 }; // Lineage2Ver111
-  std::array<char, 28> version;
-  file.read(version.data(), 28);
+    0x56, 0x00, 0x65, 0x00, 0x72, 0x00
+  }; // Lineage2Ver111
+  std::array<char, 22> header;
+  std::array<char, 6> version;
+  file.read(header.data(), 22);
+  file.read(version.data(), 6);
 
-  if (version != target_ver) {
-    std::cerr << argv[1] << " is not version 111 or is nothingness" << "\n";
+  if (header != target_ver) {
+    std::cerr << argv[1] << " is not an encrypted Lineage II file.\n";
+    return 1;
+  }
+
+  std::string ver;
+  ver += version[0];
+  ver += version[2];
+  ver += version[4];
+
+  std::istringstream out(ver);
+  int archive_version;
+  out >> archive_version;
+  if (!out) {
+    std::cerr << argv[1] << " does not have a valid version.\n";
+    return 1;
+  }
+
+  std::uint8_t key;
+  switch (archive_version) {
+  case 111: key = 0xAC; break;
+  case 121: key = 0x89; break;
+  default:
+    std::cerr << argv[1] << " version " << archive_version
+              << " is not currently supported.\n";
     return 1;
   }
 
@@ -61,7 +88,7 @@ int main(int argc, char** argv) {
   transform_n( std::istreambuf_iterator<char>(file)
              , file_size
              , std::ostreambuf_iterator<char>(std::cout)
-             , [](char c) { return c ^ 0xAC; }
+             , [key](char c) { return c ^ key; }
              );
 
   return 0;
